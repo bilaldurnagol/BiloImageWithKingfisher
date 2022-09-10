53bc55b96b6680cf33141955078508074c1dcb7f
Bilal Durnagöl <bilaldurnagol@gmail.com>
2022-09-10 20:36:07 +0300

    Initial Commit

---
+++ BiloImageWithKingfisher/BiloImageWithKingfisherApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiloImageWithKingfisherApp.swift
+//  BiloImageWithKingfisher
+//
+//  Created by Bilal Durnagöl on 10.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct BiloImageWithKingfisherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
